6be064e6d71c504b16269ff90c2e64496ee93eed
jcduqueb <50054244+jcduqueb@users.noreply.github.com>
2019-04-27 11:43:55 -0500

    Add files via upload

---
+++ SextoPunto.m
@@ -0,0 +1,14 @@
+%Positivo o negativo
+clear all 
+clc 
+close all
+%------------------------------
+a=input('ingrese numero: ');
+if a>0
+   disp('El numero es positivo');
+elseif a==0
+    disp('El numero es 0');
+else 
+       disp('El numero es negativo')
+end
+   
