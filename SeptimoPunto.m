6f8f30384f0b1f0cda7bcea6a0277e3843f28bbf
jcduqueb <50054244+jcduqueb@users.noreply.github.com>
2019-04-27 11:45:55 -0500

    Add files via upload

---
+++ SeptimoPunto.m
@@ -0,0 +1,13 @@
+%Mayor a 100
+clear all 
+clc 
+close all
+%------------------------------
+a=input('ingrese numero: ');
+if a>100
+   disp('El numero es mayor a 100');
+
+else 
+       disp('El numero no es mayor a 100')
+end
+   
